ce61be380e4981618fde1340cb0177681ccbaa0b
i.sapa <iliasapa5@gmail.com>
2026-09-18 18:26:17 +0200

    Ready for coding

---
+++ src/main/java/com/example/ecommerce_order_service/EcommerceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce_order_service/EcommerceOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
